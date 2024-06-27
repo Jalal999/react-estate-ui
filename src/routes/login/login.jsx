f6d81bd7c8d6ce4522ea0bb06fe60b4846471ec6
Jalal999 <jmammadli2021@ada.edu.az>
2024-06-27 20:09:08 +0200

    Auth FE side is implemented
    - axios request is created to send requests for login and registration
    - localStorage is added to keep user data
    - context is added to use user data through app

--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -1,15 +1,55 @@
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import apiRequest from "../../lib/apiRequest";
+import { AuthContext } from "../../context/AuthContext";
 
 function Login() {
+  const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const { updateUser } = useContext(AuthContext)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError("")
+    setIsLoading(true)
+
+    const formData = new FormData(e.target)
+
+    const username = formData.get("username")
+    const password = formData.get("password")
+
+    console.log(username, password)
+
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        username, password
+      })
+
+      // localStorage.setItem("user", JSON.stringify(res.data))
+      updateUser(res.data)
+      
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+      setError(error.response.data.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <div className="login">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
+          <button disabled={isLoading}>Login</button>
+          {error && <span>{error}</span>}
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,48 +1,82 @@
+import { Link, useNavigate } from "react-router-dom";
 import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
+import apiRequest from "../../lib/apiRequest";
 import "./profilePage.scss";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 function ProfilePage() {
+
+  const navigate = useNavigate()
+
+  const {updateUser, currentUser} = useContext(AuthContext)
+
+  console.log('curentUser: ', currentUser)
+
+  useEffect(() => {
+    if(!currentUser) {
+      console.log("test")
+      navigate("/login")
+    }
+  }, [currentUser, navigate])
+
+  const handleLogout = async () => {
+    try {
+      await apiRequest.post("/auth/logout")
+      // localStorage.removeItem("user")
+      updateUser(null)
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="profilePage">
-      <div className="details">
-        <div className="wrapper">
-          <div className="title">
-            <h1>User Information</h1>
-            <button>Update Profile</button>
+    currentUser && (
+      <div className="profilePage">
+        <div className="details">
+          <div className="wrapper">
+            <div className="title">
+              <h1>User Information</h1>
+              <Link to="/profile/update">
+                <button>Update Profile</button>
+              </Link>
+            </div>
+            <div className="info">
+              <span>
+                Avatar:
+                <img
+                  src={currentUser.avatar || "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}
+                  alt=""
+                />
+              </span>
+              <span>
+                Username: <b>{currentUser.username}</b>
+              </span>
+              <span>
+                E-mail: <b>{currentUser.email}</b>
+              </span>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+            <div className="title">
+              <h1>My List</h1>
+              <button>Create New Post</button>
+            </div>
+            <List />
+            <div className="title">
+              <h1>Saved List</h1>
+            </div>
+            <List />
           </div>
-          <div className="info">
-            <span>
-              Avatar:
-              <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
-              />
-            </span>
-            <span>
-              Username: <b>John Doe</b>
-            </span>
-            <span>
-              E-mail: <b>john@gmail.com</b>
-            </span>
+        </div>
+        <div className="chatContainer">
+          <div className="wrapper">
+            <Chat/>
           </div>
-          <div className="title">
-            <h1>My List</h1>
-            <button>Create New Post</button>
-          </div>
-          <List />
-          <div className="title">
-            <h1>Saved List</h1>
-          </div>
-          <List />
         </div>
       </div>
-      <div className="chatContainer">
-        <div className="wrapper">
-          <Chat/>
-        </div>
-      </div>
-    </div>
+    )
   );
 }
 
